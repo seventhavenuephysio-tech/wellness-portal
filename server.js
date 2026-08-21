5798b362fad50d3621c9768311f9c1370b150eda
seventhavenuephysio-tech <seventhavenuephysio@gmail.com>
2026-08-21 14:52:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'frontend')));
@@ -31,6 +32,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server listening on port ${PORT}`);
 });
